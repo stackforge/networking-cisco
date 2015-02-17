9a766d6c0183e59433a500c6b8089e19d1c0eb58
Sandhya Dasu <sadasu@cisco.com>
2015-02-17 13:23:15 -0500

    Cisco UCS Manager vendor code for ML2 mechanism driver.
    Change-Id: I23e9db27adc6efda415ef3f3d3046a988fbf7e53

---
+++ networking_cisco/plugins/ml2/drivers/cisco/ucsm/constants.py
@@ -0,0 +1,30 @@
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. 
+
+# Supported network interface cards
+PCI_INFO_CISCO_VIC_1240 = "1137:0071"
+PCI_INFO_INTEL_82599 = "8086:10c9"
+
+VLAN_PATH = "fabric/lan"
+VLAN_COMPRESSION_TYPE = "included"
+DESCR = "Created by Openstack UCSM Mech Driver"
+PORT_PROFILESETDN = "fabric/lan/profiles"
+HIGH_PERF = "high-perf-reqd"
+NONE = "none"
+
+VNIC_PATH_PREFIX = "/vnic-"
+VLAN_PATH_PREFIX = "/if-"
+VLAN_PROFILE_PATH_PREFIX = "/net-"
+VLAN_PROFILE_NAME_PREFIX = "OS-"
+PORT_PROFILE_NAME_PREFIX = "OS-PP-"
+CLIENT_PROFILE_NAME_PREFIX = "OS-CL-"
+CLIENT_PROFILE_PATH_PREFIX = "/cl-"
+
+SERVICE_PROFILE_PATH_PREFIX = "org-root/ls-"
+ETH0 = "/ether-eth0"
+ETH1 = "/ether-eth1"
