401aa04fe9e345e9cb5e1592f122529873fc5473
Arvind Somya <asomya@cisco.com>
2015-03-27 09:13:20 -0400

    Cisco providernet extension support
    This commit adds a ML2 extension driver that can be used to
    distinguish between a provider and tenant network. If this
    driver is loaded then provider networks will carry an extra
    boolean attribute 'is_provider_network' in create_network_precommit.
    
    NOTE: This value is not stored anywhere, it will be exposed on
    network create and is upto the mechanism driver(s) to store this
    information for future use.
    
    Change-Id: I517f92db7f74f0fb6f5375f529a96a530ed68cb7

---
+++ networking_cisco/plugins/ml2/drivers/cisco/nexus/extensions/cisco_providernet.py
@@ -0,0 +1,39 @@
+# Copyright 2015 Openstack Foundation.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.api.v2 import attributes
+from neutron.db import common_db_mixin
+from neutron.extensions import providernet as pnet
+from neutron.i18n import _LI
+from neutron.plugins.ml2 import driver_api as api
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+class CiscoProviderNetDriver(api.ExtensionDriver,
+                             common_db_mixin.CommonDbMixin):
+    _supported_extension_alias = 'provider'
+
+    def initialize(self):
+        LOG.info(_LI("CiscoProviderNetDriver initialization complete"))
+
+    @property
+    def extension_alias(self):
+        return self._supported_extension_alias
+
+    def process_create_network(self, context, data, result):
+        value = data.get(pnet.SEGMENTATION_ID)
+        if value is not attributes.ATTR_NOT_SPECIFIED:
+            result['is_provider_network'] = True
