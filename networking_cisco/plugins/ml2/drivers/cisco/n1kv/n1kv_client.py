0dc04df1f3691d18b2c40b1bf9c942a4f807e2b6
Sourabh Patwardhan <sopatwar@cisco.com>
2015-07-01 14:44:47 -0700

    Cisco Nexus1000V: Cannot connect to VSM using IPv6 address
    Cisco N1KV mech driver cannot connect to VSM REST endpoint
    using IPv6. The fix involves adding validation and URL
    formatting changes.
    
    Change-Id: Iab5f94ea9a5db4d4b701edfd1e688a43266df378
    Closes-Bug: #1470649

--- networking_cisco/plugins/ml2/drivers/cisco/n1kv/n1kv_client.py
+++ networking_cisco/plugins/ml2/drivers/cisco/n1kv/n1kv_client.py
@@ -137,8 +137,8 @@ class Client(object):
                 raise cfg.RequiredOptError(opt, 'ml2_cisco_n1kv')
         # Validate the configured VSM IP addresses
         # Note: Currently only support IPv4
-        for vsm_ip in self.vsm_ips:
-            if not netaddr.valid_ipv4(vsm_ip):
+        for vsm_ip in self.n1kv_vsm_ips:
+            if not (netaddr.valid_ipv4(vsm_ip) or netaddr.valid_ipv6(vsm_ip)):
                 raise cfg.Error(_("Cisco Nexus1000V ML2 driver config: "
                                   "Invalid format for VSM IP address: %s") %
                                 vsm_ip)
@@ -484,7 +484,12 @@ class Client(object):
             headers = self._get_auth_header()
             headers['Content-Type'] = headers['Accept'] = "application/json"
         for vsm_ip in hosts:
-            vsm_action = action % vsm_ip
+            if netaddr.valid_ipv6(vsm_ip):
+                # Enclose IPv6 address in [] in the URL
+                vsm_action = action % ("[%s]" % vsm_ip)
+            else:
+                # IPv4 address
+                vsm_action = action % vsm_ip
             for attempt in range(self.max_vsm_retries + 1):
                 try:
                     LOG.debug("[VSM %(vsm)s attempt %(id)s]: Connecting.." %
