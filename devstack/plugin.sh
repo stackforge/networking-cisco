938dcfaf3f9b596ff45498650d75bb946ab46ec8
Nikolay Fedotov <nfedotov@cisco.com>
2015-07-23 18:02:06 +0300

    Remove legacy code from the CSR1kv Devstack plugin.
    Remove code borrowed from devstack/master branch long time ago.
    Now we do not need to have old version of that snippets here.
    
    Change-Id: I69a575f9a73729148dd6ea4f58aa5cdde7a33bd8
    Close-Bug: #1480807

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -16,18 +16,19 @@ if is_service_enabled net-cisco; then
         echo "Installing Networking-Cisco"
         sudo python setup.py install
 
-        if is_service_enabled cisco-fwaas; then
-            echo "Installing neutron-fwaas"
-            source $DIR_CISCO/devstack/csr1kv/cisco_fwaas
-            install_cisco_fwaas
+        if is_service_enabled q-ciscorouter && is_service_enabled ciscocfgagent; then
+            source $DIR_CISCO/devstack/csr1kv/cisco_neutron
+            neutron_cisco_router_configure_common
+            if is_service_enabled cisco-fwaas; then
+                echo "Installing neutron-fwaas"
+                source $DIR_CISCO/devstack/csr1kv/cisco_fwaas
+                install_cisco_fwaas
+                configure_cisco_fwaas
+            fi
         fi
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         if is_service_enabled q-ciscorouter && is_service_enabled ciscocfgagent; then
-            source $DIR_CISCO/devstack/csr1kv/cisco_neutron
-            if is_service_enabled cisco-fwaas; then
-                configure_cisco_fwaas
-            fi
             configure_cisco_csr_router
         fi
 
