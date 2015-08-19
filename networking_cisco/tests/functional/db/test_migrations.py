59669f3a4204bceb4e2883e99b25bd1a91f41d82
Henry Gessau <gessau@cisco.com>
2015-08-19 15:06:30 -0400

    Add alembic migrations vs. models sync test
    This is a functional test, so this change also includes adding
    the functional test environment.
    
    Related-Bug: #1470678
    
    Change-Id: I5ae7141625856d34c31f5c76b0e769f6ab216901

---
+++ networking_cisco/tests/functional/db/test_migrations.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2015 Cisco Systems, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_config import cfg
+
+from neutron.db.migration.alembic_migrations import external
+from neutron.db.migration import cli as migration
+from neutron.tests.common import base
+from neutron.tests.functional.db import test_migrations
+
+from networking_cisco.db.migration.models import head
+
+# EXTERNAL_TABLES should contain all names of tables that are not related to
+# current repo.
+EXTERNAL_TABLES = set(external.TABLES) - set(external.REPO_CISCO_TABLES)
+
+
+class _TestModelsMigrationsCisco(test_migrations._TestModelsMigrations):
+
+    def db_sync(self, engine):
+        cfg.CONF.set_override('connection', engine.url, group='database')
+        for conf in migration.get_alembic_configs():
+            self.alembic_config = conf
+            self.alembic_config.neutron_config = cfg.CONF
+            migration.do_alembic_command(conf, 'upgrade', 'heads')
+
+    def get_metadata(self):
+        return head.get_metadata()
+
+    def include_object(self, object_, name, type_, reflected, compare_to):
+        # If table with alembic head version is named differently from
+        # "alembic" then "name == 'alembic_repo'" should be added here
+        # for expunction.
+        if type_ == 'table' and (name == 'alembic' or
+                                 name == 'alembic_repo' or
+                                 name in EXTERNAL_TABLES):
+            return False
+        else:
+            return True
+
+
+class TestModelsMigrationsMysql(_TestModelsMigrationsCisco,
+                                base.MySQLTestCase):
+    pass
+
+
+# class TestModelsMigrationsPsql(_TestModelsMigrationsCisco,
+#                                base.PostgreSQLTestCase):
+#     pass
