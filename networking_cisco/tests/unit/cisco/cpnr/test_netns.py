3c6474fe6e70b238e9c724455e979369958c4d7a
deepak kukrety <dkukrety@cisco.com>
2016-02-19 11:56:49 +0530

    New neutron plugin code to support CPNR product.
     CPNR product provide a scalable DHCP DNS servers. This plugin
     replaces dnsmasq and uses CPNR DHCP/DNS to provide ipaddress,
     name resolution for the VM instances.
    
     There are three major components of this plugin:
    
     First is DHCP Agent which runs as part of "neutron-dhcp-agent"
     process. User need to configure /etc/neutron/dhcp_agent.init
     as mentioned below.
    
     # The driver used to manage the DHCP server. (string value)
     #dhcp_driver = neutron.agent.linux.dhcp.Dnsmasq
     dhcp_driver = networking_cisco.plugins.cisco.\
                   cpnr.dhcp_driver.CpnrDriver
    
     Second is dhcp_relay component which runs as process.
     It uses the python eventlet library for implementing an
     event-based DHCP relay agent. The relay process will
     consist of the following greenthreads:
    
        namespace monitor - a single thread which periodically
           (every 1 second) lists all of the network namespaces on
           the system created by the neutron DHCP agent.
           If any namespaces have been created, the namespace monitor
           thread adds that namespace's information to an internal
           data structure and then uses eventlet to spawn a
           "namespace listener" greenthread which will be responsible
           for that namespace. If any namespaces have been destroyed,
           the namespace monitor thread cleans up any state
           (and threads) associated with that namespace.
        namespace listener - one per network namespace which opens
           a UDP sockets on port 67 within the respective namespace
           and listens on port 67 for DHCP requests.
           Whenever a DHCP request is received, it is validated
           and DHCP relay agent option-82 is rewritten based on
           link-selection, server-id-override, VSS and VSS control
           option.
           The request is then sent to the DHCP server via a port
           68 UDP socket from the global namespace.
        dhcp net listener - a single thread which listens on a
           port 68 UDP socket from the global namespace for
           DHCP responses from the DHCP server. Whenever a packet is
           received it is placed on a work queue for delegation to
           one of the dhcp net worker threads.
        dhcp net worker - a pool of threads for processing packets
            from the work queue. Each packet is parsed, validated,
            and DHCP relay agent option-82 is rewritten based on
            link-selection, server-id-override, VSS and VSS control
            option.
            The packet is then sent via a port 67 UDP socket
            which corresponds to the correct network namespace.
    
     Third is dns_relay component which runs as process to
        resolve hostname queries by forwarding those queries to
        CPNR DNS server.
    
    Change-Id: I209de06a3fd68af31d94c275cfe064e97278e5bb

---
+++ networking_cisco/tests/unit/cisco/cpnr/test_netns.py
@@ -0,0 +1,78 @@
+# Copyright 2016 Cisco Systems, Inc.  All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+import mock
+import unittest
+
+from networking_cisco.plugins.cisco.cpnr.netns import iflist
+from networking_cisco.plugins.cisco.cpnr.netns import nslist
+
+
+class TestNetNs(unittest.TestCase):
+
+    @mock.patch('os.path')
+    @mock.patch('os.listdir')
+    def test_nslist(self, mock_listdir, mock_path):
+        mock_path.exists.return_value = True
+        mock_listdir.return_value = []
+        mock_listdir.return_value.append(('qdhcp-1111111-2222222-3333333'))
+        mock_listdir.return_value.append(('qdhcp-4444444-5555555-6666666'))
+        nsdirs = nslist()
+        self.assertEqual('qdhcp-1111111-2222222-3333333', nsdirs[0])
+        self.assertEqual('qdhcp-4444444-5555555-6666666', nsdirs[1])
+
+        mock_path.exists.return_value = False
+        nsdirs = nslist()
+        self.assertEqual([], nsdirs)
+
+    @mock.patch('subprocess.check_output')
+    def test_iflist(self, mock_check_output):
+        fh = open('networking_cisco/tests/unit/cisco'
+                  '/cpnr/data/ip_addr_show.txt', 'rb')
+        ip_addr_str = fh.read()
+        fh.close()
+        mock_check_output.return_value = ip_addr_str
+        interfaces = iflist()
+        name, addr, mask = interfaces[0]
+        self.assertEqual(b'lo', name)
+        self.assertEqual(b'127.0.0.1', addr)
+        self.assertEqual(b'8', mask)
+        name, addr, mask = interfaces[1]
+        self.assertEqual(b'eth0', name)
+        self.assertEqual(b'10.1.1.1', addr)
+        self.assertEqual(b'24', mask)
+
+        # check ignore option
+        interfaces = iflist(ignore=(b"lo",))
+        name, addr, mask = interfaces[0]
+        self.assertEqual(b'eth0', name)
+        self.assertEqual(b'10.1.1.1', addr)
+        self.assertEqual(b'24', mask)
+        with self.assertRaises(IndexError):
+            name, addr, mask = interfaces[1]
+
+        interfaces = iflist(ignore=(b"eth0",))
+        name, addr, mask = interfaces[0]
+        self.assertEqual(b'lo', name)
+        self.assertEqual(b'127.0.0.1', addr)
+        self.assertEqual(b'8', mask)
+        with self.assertRaises(IndexError):
+            name, addr, mask = interfaces[1]
+
+        # test with no input
+        mock_check_output.return_value = ''
+        interfaces = iflist()
+        with self.assertRaises(IndexError):
+            name, addr, mask = interfaces[0]
