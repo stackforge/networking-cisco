fff6457a89905f0177b2e23dafbb56a6ad6d4fbe
Sandhya Dasu <sadasu@cisco.com>
2017-02-10 15:24:05 -0500

    UCSM Driver retry Port Profile deletes
    When VMs attached to a Port Profile are deleted, and the neutron
    network associated with this Port Profile is deleted, the Port
    Profile might not be deleted. That is because UCSM is unaware of the
    deletion on the VMs until it syncs up the UCS Servers. During this
    time Port Profiles will not be deleted by the UCSM which thinks they
    are inuse. This fix adds a retry mechanism where the plugin keeps
    track of Port Profiles that need to be deleted and retries every 10
    minutes.
    
    Change-Id: Iedc22d80f193d9b1482abdd6d68e7bda352dfe3f
    Closes-Bug: #1476721

--- networking_cisco/backwards_compatibility.py
+++ networking_cisco/backwards_compatibility.py
@@ -28,6 +28,7 @@ from neutron import version
 NEUTRON_VERSION = StrictVersion(str(version.version_info))
 NEUTRON_NEWTON_VERSION = StrictVersion('9.0.0')
 NEUTRON_OCATA_VERSION = StrictVersion('10.0.0')
+NEUTRON_OCATA_MAJOR_MINOR = StrictVersion('10.0')
 
 n_c = __import__('neutron.common.constants', fromlist=['common.constants'])
 constants = __import__('neutron_lib.constants', fromlist=['constants'])
@@ -50,12 +51,16 @@ else:
 
 
 if NEUTRON_VERSION >= NEUTRON_OCATA_VERSION:
-    from neutron import context
     from neutron.db.models import agent as agent_model
     from neutron.db.models import l3 as l3_models
     from neutron_lib.api import extensions
     from neutron_lib.db import model_base
     from neutron_lib.plugins import directory
+
+    if NEUTRON_OCATA_MAJOR_MINOR in NEUTRON_VERSION:
+        from neutron import context
+    else:
+        from neutron_lib import context
 
     get_plugin = directory.get_plugin
     n_c_attr_names = dir(n_c)
