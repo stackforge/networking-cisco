6ecdbd42a860893fcd88913235b3f4caf874168d
Shweta P <shpadubi@cisco.com>
2017-06-15 10:24:31 -0400

    Allow port to be created and updated by drivers in a transaction
    Change-Id: I89bd477cf5b5d3ad0e542e455ca02efd1240d129

--- networking_cisco/plugins/cisco/db/l3/l3_router_appliance_db.py
+++ networking_cisco/plugins/cisco/db/l3/l3_router_appliance_db.py
@@ -452,6 +452,7 @@ class L3RouterApplianceDBMixin(extraroute_db.ExtraRoute_dbonly_mixin):
                  self.get_namespace_router_type_id(context))
         if is_ha:
             # process any HA
+            setattr(context, 'GUARD_TRANSACTION', False)
             self._add_redundancy_router_interfaces(
                 context, self._make_router_dict(r_hd_binding_db.router),
                 interface_info, self._core_plugin.get_port(context,
@@ -484,6 +485,7 @@ class L3RouterApplianceDBMixin(extraroute_db.ExtraRoute_dbonly_mixin):
                                                       port_subnet_id)
 
     def remove_router_interface(self, context, router_id, interface_info):
+        setattr(context, 'GUARD_TRANSACTION', False)
         remove_by_port, remove_by_subnet = self._validate_interface_info(
             interface_info, for_removal=True)
         e_context = context.elevated()
@@ -949,6 +951,7 @@ class L3RouterApplianceDBMixin(extraroute_db.ExtraRoute_dbonly_mixin):
            DOWN and when the ASR plugin creates the port it notifies the DB to
            change the status to ACTIVE.
         """
+        setattr(context, 'GUARD_TRANSACTION', False)
         for port_id in port_ids:
             self._core_plugin.update_port_status(context, port_id, status)
 
