22da59e8a75f15a30484f252622e656875d8c0e9
Sandhya Dasu <sadasu@cisco.com>
2017-12-08 14:34:22 -0500

    ML2 Cisco UCSM MD: Add config to control SSL certificate checks.
    The UCSM mechanism driver will perform SSL certificate checking
    while connecting to the UCSM by default. This can be turned off
    using a config variable.
    
    Change-Id: Iec8f4dbb15a2a07b7ce6c29de5353089cd81789d
    Closes-Bug: 1693533

--- networking_cisco/plugins/ml2/drivers/cisco/ucsm/config.py
+++ networking_cisco/plugins/ml2/drivers/cisco/ucsm/config.py
@@ -62,6 +62,12 @@ ml2_cisco_ucsm_opts = [
                help=_('Name of QoS Policy pre-defined in UCSM, to be '
                       'applied to all VM-FEX Port Profiles. This is '
                       'an optional parameter.')),
+    cfg.StrOpt('ucsm_https_verify',
+               default=True,
+               help=_('When set to False, the UCSM driver will not check '
+                      'the SSL certificate on the UCSM. This is a global '
+                      'config for the plugin which means that it applies to '
+                      'all UCSMs in the system.')),
 ]
 
 cfg.CONF.register_opts(ml2_cisco_ucsm_opts, "ml2_cisco_ucsm")
@@ -119,6 +125,10 @@ def parse_virtio_eth_ports():
         eth_port_list.append(const.ETH_PREFIX + str(eth_port).strip())
 
     return eth_port_list
+
+
+def get_ucsm_https_verify():
+    return cfg.CONF.ml2_cisco_ucsm.ucsm_https_verify
 
 
 class UcsmConfig(object):
