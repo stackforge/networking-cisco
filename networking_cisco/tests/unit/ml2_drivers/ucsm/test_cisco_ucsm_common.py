6daf42c75d4baf72e0d54be3e8cf1433eb365597
Sam Betts <sam@code-smash.net>
2018-01-08 11:51:45 +0000

    Move all ml2 drivers to networking_cisco/ml2_drivers
    This is the first patch in a series of patches for cleaning up the
    networking-cisco code base. This patch moves all the ml2 drivers from
    networking_cisco/plugins/ml2/drivers/cisco into
    networking_cisco/ml2_drivers, reducing the nesting of empty folders and
    removing complexity from import lines.
    
    Change-Id: Ibb4f9b204538281b8db86b0e3b67321d9612e156

---
+++ networking_cisco/tests/unit/ml2_drivers/ucsm/test_cisco_ucsm_common.py
@@ -0,0 +1,159 @@
+# Copyright 2015 Cisco Systems, Inc.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslo_config import cfg
+
+from networking_cisco.backwards_compatibility import ml2_config  # noqa
+from networking_cisco.ml2_drivers.ucsm import (config as
+    ucsm_config)
+from networking_cisco.ml2_drivers.ucsm import constants as const
+from networking_cisco.tests import base as nc_base
+
+from neutron.common import config as neutron_config
+
+from neutron.tests import base
+
+
+UCSM_IP_ADDRESS_1 = '1.1.1.1'
+UCSM_USERNAME_1 = 'username1'
+UCSM_PASSWORD_1 = 'password1'
+UCSM_VIRTIO_ETH_PORTS_1 = ['eth0, eth1']
+UCSM_HOST_LIST_1 = ['UCS-1:UCS-1-SP, UCS-2:org-root/test/ls-UCS-2-SP']
+
+UCSM_IP_ADDRESS_2 = '2.2.2.2'
+UCSM_USERNAME_2 = 'username2'
+UCSM_PASSWORD_2 = 'password2'
+UCSM_VIRTIO_ETH_PORTS_2 = ['eth2, eth3']
+
+UCSM_PHY_NETS = ['test_physnet']
+
+
+ucsm_test_config_file = """
+[ml2_cisco_ucsm]
+supported_pci_devs=thing1:thing2, thing1:thing3
+
+[ml2_cisco_ucsm_ip:1.1.1.1]
+ucsm_username=username1
+ucsm_password=password1
+ucsm_host_list=UCS-1:UCS-1-SP, UCS-2:org-root/test/ls-UCS-2-SP
+ucsm_virtio_eth_ports=eth4, eth5
+vnic_template_list=test-physnet:org-root:Test-VNIC,vnic2
+sriov_qos_policy=Test
+
+[ml2_cisco_ucsm_ip:2.2.2.2]
+ucsm_username=username2
+ucsm_password=password2
+ucsm_virtio_eth_ports=eth2, eth3
+vnic_template_list=physnet2:org-root/org-Test-Sub:Test
+
+[sriov_multivlan_trunk]
+test_network1=5, 7-9
+test_network2=500-509, 700
+"""
+
+ucsm_config_bad_pci_devs = """
+[ml2_cisco_ucsm]
+supported_pci_devs=thing:thing, thing2, thing3:thing4
+"""
+
+CONF = cfg.CONF
+
+
+class ConfigMixin(object):
+
+    """Mock config for UCSM driver."""
+
+    mocked_parser = None
+
+    def set_up_mocks(self):
+        # Mock the configuration file
+        CONF.reset()
+
+        args = ['--config-file', base.etcdir('neutron.conf')]
+        neutron_config.init(args=args)
+
+        nc_base.load_config_file(ucsm_test_config_file)
+
+        # Configure the ML2 mechanism drivers and network types
+        ml2_opts = {
+            'mechanism_drivers': ['cisco_ucsm'],
+            'tenant_network_types': ['vlan'],
+        }
+        for opt, val in ml2_opts.items():
+            ml2_config.cfg.CONF.set_override(opt, val, 'ml2')
+
+        # Configure and test the Cisco UCS Manager mechanism driver
+        config = ucsm_config.UcsmConfig()
+
+        self.assertEqual(CONF.ml2_cisco_ucsm.ucsms['1.1.1.1'].ucsm_username,
+                         UCSM_USERNAME_1)
+        self.assertEqual(CONF.ml2_cisco_ucsm.ucsms['1.1.1.1'].ucsm_password,
+                         UCSM_PASSWORD_1)
+
+        self.assertEqual(CONF.ml2_cisco_ucsm.ucsms['2.2.2.2'].ucsm_username,
+                         UCSM_USERNAME_2)
+        self.assertEqual(CONF.ml2_cisco_ucsm.ucsms['2.2.2.2'].ucsm_password,
+                         UCSM_PASSWORD_2)
+
+        expected_sp_dict = {
+            ('1.1.1.1', 'UCS-1'): ('org-root/ls-UCS-1-SP'),
+            ('1.1.1.1', 'UCS-2'): ('org-root/test/ls-UCS-2-SP'),
+        }
+        self.assertEqual(expected_sp_dict, config.ucsm_sp_dict)
+
+        self.assertEqual(config.get_vnic_template_for_ucsm_ip("1.1.1.1"),
+                         [('org-root', 'Test-VNIC,vnic2')])
+
+        self.assertEqual(
+            config.get_vnic_template_for_physnet("1.1.1.1", "test-physnet"),
+            ('org-root', 'Test-VNIC,vnic2'))
+
+        self.assertEqual(
+            config.get_vnic_template_for_physnet("2.2.2.2", "physnet2"),
+            ('org-root/org-Test-Sub', 'Test'))
+
+        self.assertTrue(config.is_vnic_template_configured())
+
+        virtio_port_list1 = (
+            CONF.ml2_cisco_ucsm.ucsms['1.1.1.1'].ucsm_virtio_eth_ports)
+        self.assertEqual(virtio_port_list1,
+                         [const.ETH_PREFIX + 'eth4',
+                          const.ETH_PREFIX + 'eth5'])
+
+        virtio_port_list2 = (
+            CONF.ml2_cisco_ucsm.ucsms['2.2.2.2'].ucsm_virtio_eth_ports)
+        self.assertEqual(virtio_port_list2,
+                         [const.ETH_PREFIX + 'eth2',
+                          const.ETH_PREFIX + 'eth3'])
+
+        self.assertEqual(config.get_sriov_qos_policy('1.1.1.1'),
+                         'Test')
+
+        self.assertEqual(
+            config.get_sriov_multivlan_trunk_config("test_network1"),
+            [5, 7, 8, 9])
+
+        self.assertEqual(
+            config.get_sriov_multivlan_trunk_config("test_network2"),
+            [500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 700])
+
+        self.assertEqual(CONF.ml2_cisco_ucsm.supported_pci_devs,
+                         ["thing1:thing2", "thing1:thing3"])
+
+    def test_support_pci_devices_bad_format(self):
+        CONF.reset()
+        nc_base.load_config_file(ucsm_config_bad_pci_devs)
+        self.assertRaises(ValueError, CONF.ml2_cisco_ucsm.get,
+                          "supported_pci_devs")
