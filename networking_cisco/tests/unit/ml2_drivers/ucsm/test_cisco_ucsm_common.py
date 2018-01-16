3ab465a8a7e6f44c1e8e2753a7b02d915f53ee26
Sam Betts <sam@code-smash.net>
2018-01-16 12:57:22 +0000

    Move configurations to common networking_cisco.config module
    In order to make networking-cisco more consistent move the
    configuration modules into a common location to allow for better
    collaboration between plugins.
    
    Change-Id: Ia17ffc9fe853e577c61a329fc066871bab95ee93

--- networking_cisco/tests/unit/ml2_drivers/ucsm/test_cisco_ucsm_common.py
+++ networking_cisco/tests/unit/ml2_drivers/ucsm/test_cisco_ucsm_common.py
@@ -16,8 +16,7 @@
 from oslo_config import cfg
 
 from networking_cisco.backwards_compatibility import ml2_config  # noqa
-from networking_cisco.ml2_drivers.ucsm import (config as
-    ucsm_config)
+from networking_cisco.config import ucsm as ucsm_config
 from networking_cisco.ml2_drivers.ucsm import constants as const
 from networking_cisco.tests import base as nc_base
 
